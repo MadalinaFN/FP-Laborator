ff4dcacbd7f4997c0413ee4b34ef03333bd85a64
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-11-03 17:17:39 +0200

    Restul impartirii

---
+++ 3. Restul impartirii/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Restul_impartirii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fie i,j,k. Să se determine restul împărţirii numărului natural i^j la k
+
+            double i, j, k, numnat, restimp;
+
+            Console.WriteLine("Introduceti valoarea pentru i ");
+            i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea pentru j ");
+            j = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti valoarea pentru k ");
+            k = int.Parse(Console.ReadLine());
+
+            numnat = Math.Pow(i, j);
+            restimp = numnat % k;
+
+            Console.WriteLine($"Restul impartirii numarului natural {numnat} este {restimp}");
+        }
+    }
+}
