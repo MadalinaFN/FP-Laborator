60f6cc4164750bd59f83e371c61de4fb3da0dc11
Madalina Florina Negrea <madalinanegrea40@gmail.com>
2020-11-25 16:58:51 +0200

    Probleme

---
+++ 35.Radiani si grade/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35.Radiani_si_grade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scrieţi o funcţie care să transforme un unghi exprimat în radiani în valoarea sa exprimată în
+            // grade şi una care să facă transformarea inversă.
+
+            int n, x;
+            Console.WriteLine("Introduceti un numar exprimat in radiani");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti un numar exprimat in grade");
+            x = int.Parse(Console.ReadLine());
+
+            Program t = new Program();
+
+            Console.WriteLine("Radianii exprimati in grade sunt {0}", t.RadInGrade(n));
+            Console.WriteLine("Gradele exprimate in radiani sunt {0}", t.GradeInRad(x));
+        }
+
+        public const double PI = 3.1415926535897931;
+
+        public double RadInGrade(int num)
+        {
+            double g;
+            g = num * (180 / Math.PI);
+            return g;
+        }
+
+        public double GradeInRad(int num)
+        {
+            double r;
+            r = num * (Math.PI / 180);
+            return r;
+        }
+    }
+}
---
+++ 39.Cub Perfect/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39.Cub_Perfect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scrieti o functie care să se verifice dacă un număr natural este sau nu cub perfect.
+
+            int n;
+            Console.WriteLine("Introduceti un numar");
+            n = int.Parse(Console.ReadLine());
+
+            Program t = new Program();
+
+            Console.WriteLine(t.CubPerfect(n));
+        }
+        public int CubPerfect(int num)
+        {
+            double p, e = 0.00000001;
+            p = Math.Pow(num * 1.0, 1.0 / 3);
+            
+            int x = (int)p;
+            if (x + 1 - p < e)
+                x++;
+
+            if (x * x * x == num)
+                Console.WriteLine("Numarul este cub perfect");
+            else
+                Console.WriteLine("Numarul nu este cub perfect");
+            return num;
+        }
+    }
+}
